f4123e846b80eae9b1d59f5754eb960d11874ef8
Colin Faivre <colin.faivre@gmail.com>
2018-11-13 10:51:22 +0100

    Add hover interactions with the menu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
     <div class="menu">
-      <h1>My New team</h1>
-      <div class="user-name">Colin Faivre</div>
+      <header>
+        <h1>My New team</h1>
+        <div class="user-name">Colin Faivre</div>
+      </header>
       <div class="channels">
         <div class="channels-title">Channels</div>
         <div class="channels-content">
-          # super-channel <br>
-          # autre-channel 
+          <div># super-channel</div>
+          <div># autre-channel</div>
         </div>
       </div>
       <div class="direct-messages">
-        <div class="direct-messages-title">
-          Direct messages
-        </div>
+        <div class="direct-messages-title">Direct messages</div>
         <div class="direct-messages-content">
-          Marine <br>
-          Colin
+          <div>Marine</div>
+          <div>Colin</div>
         </div>
       </div>
     </div>
@@ -38,31 +38,47 @@ body{
 }
 .menu {
   background-color: #4d394a;
-  width: 190px;
+  width: 220px;
   height: 100vh;
   color: #c9c1c6;
   font-size: 15px;
-  padding: 15px;
+  /* padding: 15px; */
+}
+.menu header:hover{
+  background-color: #3e313c;
+  cursor: pointer;
+  color: white;
 }
 .menu h1{
   color: white;
   font-weight: 900;
   font-size: 1rem;
   margin: 0px;
+  padding-left: 15px;
+  padding-right: 15px;
 }
-.menu .channels{
+.user-name{
+  padding-left: 15px;
+  padding-right: 15px;
+}
+.menu .channels, .menu .direct-messages{
   padding-top: 18px;
 }
-.menu .channels-title:hover{
+.menu .channels-title, .menu .direct-messages-title{
+  padding-left: 15px;
+  padding-right: 15px;
+}
+.menu .channels-title:hover, .menu .direct-messages-title:hover{
   color: white;
   cursor: pointer;
 }
-.menu .direct-messages-title:hover{
-  color: white;
-  cursor: pointer;
+.menu .channels-content > div, .menu .direct-messages-content > div{
+  padding-left: 15px;
+  padding-right: 15px;
 }
-.menu .direct-messages{
-  padding-top: 18px;
+.menu .channels-content > div:hover, .menu .direct-messages-content > div:hover{
+  background-color: #3e313c;
+  cursor: pointer;
 }
 router-view{
 
