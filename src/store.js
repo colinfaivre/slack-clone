4eee377c4de344377d864fc2aac003d3a732823b
Colin Faivre <colin.faivre@gmail.com>
2018-11-14 09:14:25 +0100

    Add chat to the state & ADD_CHAT mutation

--- src/store.js
+++ src/store.js
@@ -9,10 +9,18 @@ export default new Vuex.Store({
       'super-channel',
       'autre-channel'
     ],
+    chat: [
+      'Slack bot',
+      'Damien (You)',
+      'Colin'
+    ]
   },
   mutations: {
     ADD_CHANNEL(state, newChannel) {
       state.channels.push(newChannel)
+    },
+    ADD_CHAT(state, newChat) {
+      state.chat.push(newChat)
     },
   },
   actions: {
